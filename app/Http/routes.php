57e9066e30080018c8af28262f9f37245d4708ee
mikeHorwood <mike@wisltd.co.uk>
2015-04-08 17:18:05 +0100

    Controller Update
    Bug fix.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::resource('makers', 'MakerController'), ['except' => ['create', 'edit']];
+Route::resource('makers', 'MakerController', ['except' => ['create', 'edit']]);
 
-Route::resource('vehicles', 'VehicleController', ['only' =>['index']]);
+Route::resource('vehicles', 'VehicleController', ['only' => ['index']]);
 
 Route::resource('makers.vehicles', 'MakerVehiclesController' ['except' => ['edit', 'create']]);
