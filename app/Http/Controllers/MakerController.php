d4e2c4eaf37d1b758eb83eb0bede7d16282d4b0c
mikeHorwood <mike@wisltd.co.uk>
2015-04-14 17:38:07 +0100

    Implementing store method for maker

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -4,6 +4,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Maker;
+use App\Http\Requests\CreateMakerRequest;
 
 class MakerController extends Controller
 {
@@ -25,9 +26,13 @@ class MakerController extends Controller
      *
      * @return Response
      */
-    public function store()
+    public function store(CreateMakerRequest $request)
     {
-        //
+        $values = $request->only(['name', 'phone']);
+
+        Maker::create($values);
+
+        return response()->json(['message' => 'Maker correctly added'], 201);
     }
 
     /**
