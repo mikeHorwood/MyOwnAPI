edefcfd884ba39c9a0972fab7d867a1941434439
mikeHorwood <mike@wisltd.co.uk>
2015-04-20 15:13:32 +0100

    Handling not found and unexpected exceptions
    Implementing another way to return exception responses in our API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -36,6 +38,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+
+        if($e instanceof NotfoundHttpException)
+        {
+            return response()->json(['message' => 'Bad request, please verify your request route', 'code' => 400], 400);
+        }
+        else
+        {
+            return response()->json(['message' => 'Unexpected error, try again later', 'code' => 500], 500);
+        }
     }
 }
